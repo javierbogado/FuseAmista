585f5bab638c241c1f68cc4d8a5627d82710218a
ccontarino <ccontarino@gmail.com>
2017-09-20 13:20:43 -0300

    publicacion cxf

--- GestionCompDemo/src/main/java/org/mycompany/rest/service/FileService.java
+++ GestionCompDemo/src/main/java/org/mycompany/rest/service/FileService.java
@@ -1,11 +1,12 @@
-package org.mycompany.service;
+package org.mycompany.rest.service;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+
+import org.springframework.stereotype.Component;
 
 // START SNIPPET: example
 @Path("/fileservice/")
